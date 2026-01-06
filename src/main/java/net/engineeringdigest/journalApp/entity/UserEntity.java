83e95951601e2ecde1987372e2b8a24989e17b25
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2026-01-06 23:22:33 +0530

    Added UserEntity

--- src/main/java/net/engineeringdigest/journalApp/entity/UserEntity.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/UserEntity.java
@@ -4,7 +4,11 @@ import lombok.Data;
 import lombok.NonNull;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Document(collection = "users")
 @Data
@@ -17,4 +21,7 @@ public class UserEntity {
     private String userName;
     @NonNull
     private String password;
+    @DBRef
+    private List<JournalEntity> journalEntries = new ArrayList<>();
+
 }
