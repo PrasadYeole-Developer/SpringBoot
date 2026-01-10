4e150e0db177b3ec021bdb8ab858aaee40966fa8
Sarvadnya Yeole <yeolesv1012@gmail.com>
2026-01-10 17:33:03 +0530

    Configured SpringSecurity in Config folder

---
+++ src/main/java/net/engineeringdigest/journalApp/config/SpringSecurity.java
@@ -0,0 +1,11 @@
+package net.engineeringdigest.journalApp.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SpringSecurity extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception{
+
+    }
+}
